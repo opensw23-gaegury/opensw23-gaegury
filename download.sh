3b18279cb4c13b6560853c2e1ee98ecd83aa8b1f
songhyun9999 <songhyun0323@konkuk.ac.kr>
2023-06-01 16:10:01 +0900

    edit download.sh

--- download.sh
+++ download.sh
@@ -5,9 +5,6 @@
 mkdir -p data/pretrained
 cd data/pretrained
 
-curl https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_classifier.pt # Trained by OpenAI
-curl https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion.pt # Trained by OpenAI
-
 gdown https://drive.google.com/uc?id=1norNWWGYP3EZ_o05DmoW1ryKuKMmhlCX
 gdown https://drive.google.com/uc?id=1QEl-btGbzQz6IwkXiFGd49uQNTUtTHsk
 )
